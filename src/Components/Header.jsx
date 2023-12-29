fa921e81c420620e309696db549a7978070cc2ad
thummar05 <152049622+thummar05@users.noreply.github.com>
2023-12-29 21:38:41 +0530

    Update Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='border shadow-md py-4 fixed top-0 bg-white w-full text-center'>
-      <header className='text-3xl font-bold uppercase'>Codehelp Blogs</header> 
+      <header className='text-3xl font-bold uppercase'>Blogs</header> 
     </div>
   )
 }
